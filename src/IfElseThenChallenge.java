e339c8a64be610bac2b231b59a402417c1d4feb0
Anas Razy <razy7292@gmail.com>
2025-06-20 17:59:03 +0530

    IfElseBasics

---
+++ src/IfElseThenChallenge.java
@@ -0,0 +1,5 @@
+public class IfElseThenChallenge {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // this is a valid java statement
+        int int4 = 10;
+        // java expression is int4 = 10 (without data type or semicolon)
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a Test");
+        System.out.println("This is " + "another "+ "still more.");
+
+        // without vertical whitespace
+        int anotherVariable = 50; myVariable--; System.out.println(" myVariable = "+ myVariable);
+
+
     }
 }
