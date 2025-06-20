5a307f5c8824a5f9c814b642ca22d3bd392d9a01
Anas Razy <razy7292@gmail.com>
2025-06-20 18:25:53 +0530

    Methods Basics

--- src/MethodsInJava.java
+++ src/MethodsInJava.java
@@ -1,5 +1,23 @@
 public class MethodsInJava {
     public static void main(String[] args) {
         // Learning Method
+
+        int levelCompleted = 5;
+        int bonus = 200;
+        calculateScore(true, 800, levelCompleted, bonus);
+
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+//        boolean gameOver = true;
+//        int score =  800;
+//        int levelCompleted = 5;
+//        int bonus = 200;
+        int finalScore = score;
+        if (gameOver){
+            finalScore += (levelCompleted * bonus);
+            finalScore+=1000;
+            System.out.println("Your final score was "+ finalScore);
+        }
     }
 }
