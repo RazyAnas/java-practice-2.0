01fd145cf7be126956d3e78847ba95fc98ad6a15
Anas Razy <razy7292@gmail.com>
2025-06-20 14:03:48 +0530

    Adding other classes

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Tim");
+
+        boolean isAlien = false;
+
+        // Check Alien or not an Alien
+        if (!isAlien) {
+            System.out.println("No, It is not an Alien!");
+            System.out.println("And I am scared of Aliens.");
+        }
+    }
+}
