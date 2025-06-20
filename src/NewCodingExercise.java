39a97af9f02c1e011b2255713c2d37decdcaa47a
Anas Razy <razy7292@gmail.com>
2025-06-20 19:53:52 +0530

    New Coding Exercise done

--- src/NewCodingExercise.java
+++ src/NewCodingExercise.java
@@ -1,5 +1,30 @@
 public class NewCodingExercise {
-    public static void main(String[] args) {
+ /*
+ Positive, Negative or Zero
+Instructions
 
+Write a method called checkNumber with an int parameter named number.
+
+
+The method should not return any value, and it needs to print out:
+
+    - "positive" if the parameter number is > 0
+
+    - "negative" if the parameter number is < 0
+
+    - "zero" if the parameter number is equal to 0
+
+
+NOTE:  The checkNumber method needs to be defined as public static like we have been doing so far in the course.
+
+NOTE:  Do not add a main method to your solution code!
+
+  */
+// public static void main(String[] args) {
+//     checkNumber(0);
+// }
+    public static void checkNumber(int number){
+        // condition ? match : or else
+        System.out.println(number > 0 ? "positive" : (number < 0 ? "negative" : "zero"));
     }
 }
