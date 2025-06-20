5c96309804bfae2fc2a664569470d7acda921ee9
Anas Razy <razy7292@gmail.com>
2025-06-21 00:02:29 +0530

    Method Overloading Challenge Done.

--- src/MethodOverloading.java
+++ src/MethodOverloading.java
@@ -1,5 +1,14 @@
 public class MethodOverloading {
     public static void main(String[] args) {
-        // already done!
+        System.out.println(convertToCentimeters(68));
+        System.out.println(convertToCentimeters(5,8));
     }
+    public static double convertToCentimeters(int inches){
+        return inches * (1/0.3937);
+    }
+    public static double convertToCentimeters(int feet, int inches){
+        int onlyInches = feet*12 + inches;
+        return convertToCentimeters(onlyInches);
+    }
+
 }
