302698c66f97b2bb1f2f0182bcddc219d7f3feda
Anas Razy <razy7292@gmail.com>
2025-06-21 12:17:28 +0530

    MinutesToYear+days Done.

---
+++ src/MinutesToYearsDaysCalculator.java
@@ -0,0 +1,15 @@
+public class MinutesToYearsDaysCalculator {
+    public static void main(String[] args) {
+        printYearsAndDays(561600);
+    }
+    public static void printYearsAndDays(long minutes){
+        int years = (int) (minutes/525600);
+        int days = (int)((minutes%525600)/1440);
+        if (minutes < 0){
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(minutes+" min = "+years+" y and "+days+" d");
+        }
+
+    }
+}
