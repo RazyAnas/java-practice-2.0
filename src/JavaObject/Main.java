a9d1408f2128a1938e3355d95a1aeb5e2d6bc4a7
Anas Razy <razy7292@gmail.com>
2025-06-23 21:48:43 +0530

    Java Object

--- src/JavaObject/Main.java
+++ src/JavaObject/Main.java
@@ -1,4 +1,49 @@
 package JavaObject;
 
 public class Main extends java.lang.Object{
+    public static void main(String[] args) {
+        Student max = new Student("Max", 21);
+        System.out.println(max.toString());
+
+        PrimarySchoolStudent paglu = new PrimarySchoolStudent("Paglu Khan", 10, "Paglu ka baap maha paglu");
+        System.out.println(paglu.toString());
+    }
+}
+class Student{
+    private  String name;
+    private  int age;
+
+    Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+//    @Override
+//    public String toString() {
+//        return super.toString();
+//    }
+
+
+    @Override
+    public String toString() {
+        return  name + " is " + age;
+//        return "Student{" +
+//                "name='" + name + '\'' +
+//                ", age=" + age +
+//                '}';
+    }
+}
+
+class PrimarySchoolStudent extends Student{
+    private String parentName;
+
+    public PrimarySchoolStudent(String name, int age, String parentName) {
+        super(name, age);
+        this.parentName = parentName;
+    }
+
+    @Override
+    public String toString() {
+        return parentName + " ka beta, " + super.toString();
+    }
 }
