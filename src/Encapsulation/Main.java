eeb75a7b1231ec33a87fec557c99e798429dad24
Anas Razy <razy7292@gmail.com>
2025-06-24 19:42:28 +0530

    Encapsulation

---
+++ src/Encapsulation/Main.java
@@ -0,0 +1,44 @@
+package Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.name = "Tim";
+        player.health = 20;
+        player.weapon = "Sword";
+
+        int damage =10;
+        player.loseHealth(damage);
+
+        System.out.println("Remaining health = " + player.healthRemaining());
+        player.health = 200;
+        player.loseHealth(11);
+        System.out.println("Remaining health = " + player.healthRemaining());
+    }
+}
+
+class Player{
+    public String fullName;
+    public int health;
+    public String weapon;
+
+    public void loseHealth(int damage) {
+        this.health = health - damage;
+        if (health <= 0) {
+            System.out.println("Player knocked out of the game!");
+        }
+    }
+
+    public int healthRemaining(){
+        return health;
+    }
+
+    public void restoreHealth(int extraHealth) {
+        this.health = health + extraHealth;
+        if (health > 100){
+            System.out.println("Player restored to 100%");
+            this.health = 100;
+        }
+    }
+
+}
