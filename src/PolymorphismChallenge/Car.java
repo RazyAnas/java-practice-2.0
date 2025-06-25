39d44dff13b0d13c42f2a66b6ffaee3c6fa14af1
Anas Razy <razy7292@gmail.com>
2025-06-25 11:47:03 +0530

    Polymorphism Challenge

---
+++ src/PolymorphismChallenge/Car.java
@@ -0,0 +1,42 @@
+package PolymorphismChallenge;
+
+public class Car {
+
+    // four required variable interfaces
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+    }
+
+    // default values for all cars
+    public Car() {
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+    // five other additional methods as required in question
+    public String startEngine() {
+
+        return startEngine().getClass().getSimpleName() + ": Car engine is starting...";
+    }
+    public String accelerate() {
+        return accelerate().getClass().getSimpleName() + ": The car is accelerating...";
+    }
+    public String brake() {
+        return brake().getClass().getSimpleName() + ": The car is braking...";
+    }
+
+    // two getter methods should also be defined!
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/PolymorphismChallenge/Ford.java
@@ -0,0 +1,22 @@
+package PolymorphismChallenge;
+
+public class Ford extends Car{
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String brake() {
+        return super.brake();
+    }
+
+    @Override
+    public String accelerate() {
+        return super.accelerate();
+    }
+
+    @Override
+    public String startEngine() {
+        return super.startEngine();
+    }
+}
