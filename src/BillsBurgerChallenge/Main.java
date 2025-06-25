0fd431e22d152a08965b7c818f227a633b4bc967
Anas Razy <razy7292@gmail.com>
2025-06-25 23:01:18 +0530

    Bill's Burger Challenge

--- src/BillsBurgerChallenge/Main.java
+++ src/BillsBurgerChallenge/Main.java
@@ -2,6 +2,28 @@ package BillsBurgerChallenge;
 
 public class Main {
     public static void main(String[] args) {
+//        Item coke = new Item("drink", "coke", 1.50);
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
+//
+//        Item avocado = new Item("Topping", "avocado", 1.50);
+//        avocado.printItem();
+
+//        Burger burger = new Burger("regular", 4.00);
+//        burger.addToppings("BACON", "CHEESE", "MAYO");
+//        burger.printItem();
+
+//        MealOrder regularMeal = new MealOrder();
+//        regularMeal.addBurgerToppings("BACON", "CHEESE","MAYO");
+//        regularMeal.setDrinkSize("MEDIUM");
+//        regularMeal.printItemizedList();
+
+        MealOrder secondMeal = new MealOrder("turkey", "7-up", "chilli");
+        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
+        secondMeal.setDrinkSize("SMALL");
+        secondMeal.printItemizedList();
+
 
     }
 }
