68c50b76c4e3fa32b741eb69de7fdbcc1ab9db8f
Anas Razy <razy7292@gmail.com>
2025-06-26 13:20:47 +0530

    Array with random number

---
+++ src/UsingArrays/Main.java
@@ -0,0 +1,21 @@
+package UsingArrays;
+
+import java.util.Arrays;
+import java.util.Formattable;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] firstArray = getRandomArray(10);
+        System.out.println(Arrays.toString(firstArray));
+    }
+
+    private static int[] getRandomArray(int len) {
+        Random random = new Random();
+        int[] newInt = new int[len];
+        for (int i = 0; i < len; i++) {
+            newInt[i] = random.nextInt(100);
+        }
+        return newInt;
+    }
+}
