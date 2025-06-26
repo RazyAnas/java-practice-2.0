b30dca4a5a958ce408896e78502c499842944095
Anas Razy <razy7292@gmail.com>
2025-06-26 20:17:01 +0530

    Reverse Array challenge

--- src/ReverseArrayChallenge/Main.java
+++ src/ReverseArrayChallenge/Main.java
@@ -5,23 +5,23 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] arr = {15, 2, 6, 8, 5};
-        System.out.println("Before reverse our array was: " + Arrays.toString(arr));
-        System.out.println();
+        System.out.println("Array = " + Arrays.toString(arr));
+        reverse(arr);
+        System.out.println("Reversed array = " + Arrays.toString(arr));
 
-        System.out.println("After reverse our array become: " + Arrays.toString(reverse(arr)));
     }
 
-    public static int[] reverse(int... args){
-//        System.out.println(Arrays.toString(args));
-        for (int i = 0; i < args.length - 1 ; i++){
+    private static void reverse(int[] array){
+//        System.out.println(Arrays.toString(array));
+        for (int i = 0; i < array.length - 1 ; i++){
 
-            for (int j = i+1; j < args.length; j++) {
-                int temp = args[i];
-                args[i] = args[j];
-                args[j] = temp;
+            for (int j = i+1; j < array.length; j++) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
             }
 
         }
-        return args;
+
     }
 }
