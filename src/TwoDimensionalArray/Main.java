89a34dfe2005504e59073ab402cd60f95470ef32
Anas Razy <razy7292@gmail.com>
2025-06-26 20:45:44 +0530

    2D Array

---
+++ src/TwoDimensionalArray/Main.java
@@ -0,0 +1,23 @@
+package TwoDimensionalArray;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        System.out.println(Arrays.toString(array));
+        System.out.println("array.length = " + array.length);
+
+        for (int[] outer : array) {
+            System.out.println(Arrays.toString(outer));
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            var innerArray = array[i];
+            for (int j = 0; j < innerArray.length; j++) {
+                System.out.println(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
