10b02772e2f20bf8055fe8ec161b704a0ff0a2a2
Anas Razy <razy7292@gmail.com>
2025-06-27 13:50:01 +0530

    ArrayList

--- src/ArrayLists/MoreList.java
+++ src/ArrayLists/MoreList.java
@@ -1,6 +1,8 @@
 package ArrayLists;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class MoreList {
@@ -20,5 +22,45 @@ public class MoreList {
 
         groceries.addAll(nextList);
         System.out.println(groceries);
+
+        System.out.println("Third item = " + groceries.get(2));
+
+        if (groceries.contains("mustard")) {
+            System.out.println("List contains mustard");
+        }
+
+        groceries.add("yogurt");
+        System.out.println("first = " + groceries.indexOf("yogurt"));
+        System.out.println("first = " + groceries.lastIndexOf("yogurt"));
+
+        System.out.println(groceries);
+        groceries.remove(1);
+        System.out.println(groceries);
+        groceries.remove("yogurt");
+        System.out.println(groceries);
+
+        groceries.removeAll(List.of("apples", "eggs"));
+        System.out.println(groceries);
+
+        groceries.retainAll((List.of("apples", "milk", "mustard", "cheese")));
+        System.out.println(groceries);
+
+        groceries.clear();
+        System.out.println(groceries);
+        System.out.println("isEmpty = " + groceries.isEmpty());
+
+        groceries.addAll(List.of("apples", "milk", "mustard", "cheese"));
+        groceries.addAll(Arrays.asList("eggs", "pickles", "mustard", "ham"));
+        System.out.println(groceries);
+        groceries.sort(Comparator.naturalOrder());
+        System.out.println(groceries);
+        groceries.sort(Comparator.reverseOrder());
+        System.out.println(groceries);
+
+        // List ➝ Array (List to Array)
+        var grocerArray = groceries.toArray(new String[groceries.size()]);
+        System.out.println(Arrays.toString(grocerArray));
+
+
     }
 }
