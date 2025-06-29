fcf2c09f545e09b88494588d3eae51fed1f46a5c
Anas Razy <razy7292@gmail.com>
2025-06-29 17:07:20 +0530

    playlist challenge done

--- src/Playlist/Album.java
+++ src/Playlist/Album.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 public class Album {
     private final String albumName;
 //    private final Song song; // "HAS A" song
-    LinkedList<Song> songLinkedList = new LinkedList<>(); // HAS A SONG LIST
+    LinkedList<Song> songLinkedList = new LinkedList<>(); // "HAS A" SONG LIST
 
     public Album(String albumName, LinkedList<Song> songLinkedList) {
         this.albumName = albumName;
