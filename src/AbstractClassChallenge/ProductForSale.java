f4e77a6272f1425e5f5a1e5a35c14b588c1b181d
Anas Razy <razy7292@gmail.com>
2025-06-30 15:44:49 +0530

    AbstractClass Challenge done

--- src/AbstractClassChallenge/ProductForSale.java
+++ src/AbstractClassChallenge/ProductForSale.java
@@ -4,9 +4,13 @@ package AbstractClassChallenge;
 public abstract class ProductForSale {
 
     // three fields type, price and a description
-    private String type;
-    private final double price;
-    private String description;
+    String type;
+    double price;
+    String description;
+
+    public String getType() {
+        return type;
+    }
 
     public ProductForSale(String type, double price, String description) {
         this.type = type;
@@ -23,24 +27,91 @@ public abstract class ProductForSale {
     // method to: printPricedItem (concrete) --> takes quantity and print itemized line item order with quantity and line-item price
     // like this: 3 x Pen @ ₹10 = ₹30
     public String printPricedItem(int quantity) {
-
         return quantity + " x " + type + " @ " + "₹" + price + " = " + "₹" + getSalesPrice(quantity);
     }
 
     // method to: showDetails (abstract) --> represents what might be displayed on product page: Product type, description, price, so on
     public abstract void showDetails();
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{" +
+                "type='" + type + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
 
 // also create an OrderItem type that has two fields: quantity, ProductForSale
 class OrderItem {
-
-    private int quantity;
-    private ProductForSale productForSale;
+    private final int quantity;
+    ProductForSale productForSale; // OrderItem "HAS-A" productForSale
 
     public OrderItem(int quantity, ProductForSale productForSale) {
         this.quantity = quantity;
         this.productForSale = productForSale;
     }
+
+    public double getPrice() {
+        return productForSale.getSalesPrice(quantity);
+    }
+
+    @Override
+    public String toString() {
+        return productForSale.printPricedItem(quantity);
+    }
 }
 
 // also create two or three classes that extends ProductForSale, that will be your products in your store
+class Toy extends ProductForSale {
+
+    public Toy(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+
+        System.out.println("Type: " + type);
+        System.out.println("Price: ₹" + price);
+        System.out.println("Description: " + description);
+        System.out.println();
+    }
+
+}
+
+class HomeDecor extends ProductForSale {
+
+    public HomeDecor(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+
+        System.out.println("Type: " + type);
+        System.out.println("Price: ₹" + price);
+        System.out.println("Description: " + description);
+        System.out.println();
+    }
+
+}
+
+class Electronics extends ProductForSale {
+
+    public Electronics(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+
+        System.out.println("Type: " + type);
+        System.out.println("Price: ₹" + price);
+        System.out.println("Description: " + description);
+        System.out.println();
+    }
+
+}
+
--- src/AbstractClassChallenge/Store.java
+++ src/AbstractClassChallenge/Store.java
@@ -4,9 +4,53 @@ package AbstractClassChallenge;
 // manage an order, which can just be a "list of OrderItem" objects
 // have methods to "add an item to the order", and "print the ordered items", so it looks like a sales receipt
 
+import java.util.ArrayList;
+
 public class Store {
 
+    static ArrayList<ProductForSale> products = new ArrayList<>(); // "list of products for sale"
+    static ArrayList<OrderItem> orderList = new ArrayList<>(); // making order list
     public static void main(String[] args) {
 
+        // defining our store stock
+        products.add(new Toy("Rubik's Cube", 450.3, "ETERNAL Cube 3x3 High Speed Stickerless Magic Cubes Puzzle Game Toy"));
+        products.add(new Toy("Xylophone", 281, "Learn With Fun 2 in 1 Xylophone and Piano Toy  (Multicolor)"));
+        products.add(new HomeDecor("Artificial Plant", 250, "Naturally Bonsai for home decoration,office decor, cafe, (4) Bonsai Wild Artificial Plant with Pot  (20 cm, Multicolor)"));
+        products.add(new Electronics("Trimmer", 899, "Mi by Xiaomi Beard 2C Trimmer 90 min Runtime 40 Length Settings  (Black)"));
+        products.add(new Electronics("Apple iPhone", 84_900.9, "Apple iPhone 16 (Teal, 256 GB)"));
+
+        System.out.println("\nStore Stock:");
+        displayStoreProducts();
+
+        System.out.println();
+
+        // adding item to cart
+        addItemToOrder(2, 0); // 2 - rubik's cube
+        addItemToOrder(1, 4); // 1 - iPhone
+        addItemToOrder(3, 3); // 3 - trimmer
+
+        // print the full bill
+        double totalPrice = 0;
+        System.out.println("Your Order:");
+        for (OrderItem item : orderList) {
+            System.out.println(item);
+            totalPrice += item.getPrice();
+        }
+
+        // checkout and total
+        System.out.printf("_".repeat(40) + "\nTOTAL PRICE = ₹%s\n",totalPrice);
     }
+
+    public static void addItemToOrder(int quantity, int productIndex) { // "add an item to the order"
+        // order items from stock
+        orderList.add(new OrderItem(quantity, products.get(productIndex)));
+
+    }
+
+    public static void displayStoreProducts() {
+        for (ProductForSale product : products) {
+            product.showDetails();
+        }
+    }
+
 }
