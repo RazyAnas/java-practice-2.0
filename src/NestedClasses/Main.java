68c9f3ec312a7eb9464c9f457247072945bc1b18
Anas Razy <razy7292@gmail.com>
2025-07-03 20:21:31 +0530

    Inner Class

--- src/NestedClasses/Main.java
+++ src/NestedClasses/Main.java
@@ -1,6 +1,7 @@
 package NestedClasses;
 
 import NestedClasses.domain.Employee;
+import NestedClasses.domain.StoreEmployee;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +24,30 @@ public class Main {
                 .EmployeeComparator<>("yearStarted").reversed());
 
         for (Employee e : employees) {
+            System.out.println(e);
+        }
+
+        System.out.println("Store Members");
+
+        List<StoreEmployee> storeEmployees = new ArrayList<>(List.of(
+                new StoreEmployee(10015, "Meg",
+                        2019, "Target"),
+                new StoreEmployee(10515, "Joe",
+                        2019, "Walmart"),
+                new StoreEmployee(10105, "Tom",
+                        2020, "Macys"),
+                new StoreEmployee(10215, "Marty",
+                        2018, "Walmart"),
+                new StoreEmployee(10322, "Bud",
+                        2016, "Target")
+        ));
+
+//        var comparator = new Employee.EmployeeComparator<>();
+
+        var comparator =  new StoreEmployee().new StoreComparator<>();
+        storeEmployees.sort(comparator);
+
+        for (StoreEmployee e : storeEmployees) {
             System.out.println(e);
         }
 
