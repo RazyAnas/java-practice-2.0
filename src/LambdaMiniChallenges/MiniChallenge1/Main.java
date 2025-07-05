789271c5efb41ebde7401d53007d2f8c68487d13
Anas Razy <razy7292@gmail.com>
2025-07-05 18:30:10 +0530

    Lambda Mini Challenges 1 done

--- src/LambdaMiniChallenges/MiniChallenge1/Main.java
+++ src/LambdaMiniChallenges/MiniChallenge1/Main.java
@@ -1,8 +1,31 @@
 package LambdaMiniChallenges.MiniChallenge1;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        // without lambda
+        Consumer<String> printWords = new Consumer<String>() {
+            @Override
+            public void accept(String sentence) {
+                String[] parts  = sentence.split(" ");
+                for (String part : parts) {
+                    System.out.println(part);
+                }
+            }
+        };
+
+        // with lambda expression
+        Consumer<String> printWordsLambda = sentence -> {
+            List<String> parts = List.of(sentence.split(" "));
+            parts.forEach(System.out::println);
+        };
+
+        printWords.accept("Let's split this up into an array");
+        printWordsLambda.accept("Let's split this up into an array");
 
 
     }
