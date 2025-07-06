e44847dd470f338dd55e77f44ff5019d6b0769b0
Anas Razy <razy7292@gmail.com>
2025-07-06 19:52:54 +0530

    Cards

---
+++ src/CardGameChallenge/Main.java
@@ -0,0 +1,23 @@
+package CardGameChallenge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+🃏 What is War?
+A two-player game where:
+    Each player is dealt half the deck (26 cards each).
+    In each round, both players flip their top card.
+    The player with the higher card wins the round and takes both cards to the bottom of their deck.
+    If the cards are equal → it's WAR! (tie-breaker round).
+    Game ends when one player has all the cards, or you stop after a number of rounds.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // create deck
+        List<Card> deck = new ArrayList<>();
+        deck
+    }
+}
--- src/CollectionsMethods/Main.java
+++ src/CollectionsMethods/Main.java
@@ -72,6 +72,58 @@ public class Main {
         boolean disjoint2 = Collections.disjoint(kings, tens);
         System.out.println("disjoint = " + disjoint2);
 
+        deck.sort(sortingAlgorithm);
+        Card tenOfHearts = Card.getNumericCard(Card.Suit.HEART, 10);
+        // list has to be sorted before binarySearch to work
+        int foundIndex = Collections.binarySearch(deck, tenOfHearts, sortingAlgorithm);
+        System.out.println("foundIndex = " + foundIndex);
+        System.out.println("foundIndex = " + deck.indexOf(tenOfHearts)); // sorted or not gives the result
+        System.out.println(deck.get(foundIndex));
+
+        Card tenOfClubs = Card.getNumericCard(Card.Suit.CLUB, 10);
+        Collections.replaceAll(deck, tenOfClubs, tenOfHearts);
+        Card.printDeck(deck.subList(32, 36), "Tens row", 1);
+
+        Collections.replaceAll(deck, tenOfHearts, tenOfClubs);
+        Card.printDeck(deck.subList(32, 36), "Tens row", 1);
+
+        if (Collections.replaceAll(deck, tenOfHearts, tenOfClubs)) {
+            System.out.println("Tens of hearts replaced with tens of clubs");
+        } else {
+            System.out.println("No tens of hearts found in the list"); // because we replaced with tens of clubs
+        }
+
+        System.out.println("Ten of Clubs Cards = " +
+                Collections.frequency(deck, tenOfClubs)); // allows to check for duplicates
+        System.out.println("Best Card = " + Collections.max(deck, sortingAlgorithm));
+        System.out.println("Worst Card = " + Collections.min(deck, sortingAlgorithm));
+
+        // sort by first suit and then rank
+        var sortBySuit = Comparator.comparing(Card::suit)
+                .thenComparing(Card::rank);
+        deck.sort(sortBySuit);
+        Card.printDeck(deck, "Sorted by Suit, Rank", 4);
+
+        List<Card> copied = new ArrayList<>(deck.subList(0,13)); // just clubs cards
+        Collections.rotate(copied, 2); // last two becomes the first two
+        System.out.println("UnRotated: " + deck.subList(0,13));
+        System.out.println("Rotated " + 2 + ": " + copied);
+
+        copied = new ArrayList<>(deck.subList(0,13)); // just clubs cards
+        Collections.rotate(copied, -2); // first two becomes the last two
+        System.out.println("UnRotated: " + deck.subList(0,13));
+        System.out.println("Rotated " + -2 + ": " + copied);
+
+        copied = new ArrayList<>(deck.subList(0,13)); // just clubs cards
+        for (int i = 0; i < copied.size()/2; i++) {
+            Collections.swap(copied, i, copied.size() - 1 - i);
+        }
+        System.out.println("Manual reverse: " + copied);
+
+        copied = new ArrayList<>(deck.subList(0,13)); // just clubs cards
+        Collections.reverse(copied);
+        System.out.println("Automatic reverse: " + copied);
 
     }
+
 }
