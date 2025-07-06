f5e79daacce1019410d46d5783ec9a4de79581a8
Anas Razy <razy7292@gmail.com>
2025-07-06 16:58:03 +0530

    nCopies

--- src/CollectionsMethods/Main.java
+++ src/CollectionsMethods/Main.java
@@ -1,9 +1,44 @@
 package CollectionsMethods;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Card.printDeck(Card.getStandardDeck());
+        Card.printDeck(Card.getStandardDeck()); // gives 52 cards in 4 suits
+
+        Card[] cardArray = new Card[13];
+        Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART, 'A');
+        Arrays.fill(cardArray, aceOfHearts);
+        Card.printDeck(Arrays.asList(cardArray), "Aces of Hearts", 1);
+
+        List<Card> cards = new ArrayList<>(52); // it just sets the capacity of 52, does not populate the elements
+        Collections.fill(cards, aceOfHearts); // fill method doesn't actually add elements if my list is empty, it stays empty
+        System.out.println(cards);
+        System.out.println("cards.size() = " + cards.size());
+
+        List<Card> acesOfHearts = Collections.nCopies(13, aceOfHearts);
+        Card.printDeck(acesOfHearts, "Aces of Hearts", 1);
+
+        Card kingOfClubs = Card.getFaceCard(Card.Suit.CLUB, 'K');
+        List<Card> kingsOfClubs = Collections.nCopies(13, kingOfClubs);
+        Card.printDeck(kingsOfClubs, "Kings of Clubs", 1);
+
+        Collections.addAll(cards, cardArray);
+        Collections.addAll(cards, cardArray);
+        Card.printDeck(cards, "Card Collection with Aces added", 2);
+
+        // Source does not fit in dest --> problem = cards: 0, kingsOfClubs = 13 destination has fewer elements than source
+        Collections.copy(cards, kingsOfClubs);
+        Card.printDeck(cards, "Card Collection with Kings copied", 1);
+
+        // this method returns unmodifiable list --> if we want modifiable we need to call copyOf method
+        cards = List.copyOf(kingsOfClubs);
+        Card.printDeck(cards, "List Copy of Kings", 1);
+
     }
 }
