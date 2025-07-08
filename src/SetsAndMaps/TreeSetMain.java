c7b548aa75048055bf934bb5e3303e181933425e
Anas Razy <razy7292@gmail.com>
2025-07-08 22:04:34 +0530

    TreeSet introduction

---
+++ src/SetsAndMaps/TreeSetMain.java
@@ -0,0 +1,52 @@
+package SetsAndMaps;
+
+import java.util.*;
+
+public class TreeSetMain {
+
+    public static void main(String[] args) {
+        String divider = "_".repeat(30);
+        List<Contact> phones = ContactData.getData("phone");
+        List<Contact> emails = ContactData.getData("email");
+
+//        NavigableSet<Contact> sorted = new TreeSet<>(phones);
+        Comparator<Contact> mySort = Comparator.comparing(Contact::getName);
+        NavigableSet<Contact> sorted = new TreeSet<>(mySort);
+        sorted.addAll(phones);
+        sorted.forEach(System.out::println);
+
+        NavigableSet<String> justNames = new TreeSet<>();
+        phones.forEach(c -> justNames.add(c.getName()));
+        System.out.println(justNames);
+
+        System.out.println(divider);
+
+        NavigableSet<Contact> fullSet = new TreeSet<>(sorted);
+        fullSet.addAll(emails);
+        fullSet.forEach(System.out::println);
+
+        System.out.println(divider);
+
+        List<Contact> fullList = new ArrayList<>(phones);
+        fullList.addAll(emails);
+        fullList.sort(sorted.comparator());
+        fullList.forEach(System.out::println);
+
+        Contact min = Collections.min(fullSet, fullSet.comparator());
+        Contact max = Collections.max(fullList, fullSet.comparator());
+
+        Contact first = fullSet.first();
+        Contact last = fullSet.last();
+
+        System.out.println(divider);
+        System.out.printf("min = %s, first=%s %n", min.getName(), first.getName());
+        System.out.printf("max = %s, last=%s %n", max.getName(), last.getName());
+        System.out.println(divider);
+
+        NavigableSet<Contact> copiedSet = new TreeSet<>(fullSet);
+        System.out.println("First element = " + copiedSet.pollFirst());
+        System.out.println("Last element = " + copiedSet.pollLast());
+        copiedSet.forEach(System.out::println);
+
+    }
+}
