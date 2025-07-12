5a7109e7d9847bdb9dcaa56951ec2597ddef702d
Anas Razy <razy7292@gmail.com>
2025-07-12 17:26:17 +0530

    Tree set

--- src/SetsAndMaps/TreeSetMain.java
+++ src/SetsAndMaps/TreeSetMain.java
@@ -10,7 +10,7 @@ public class TreeSetMain {
         List<Contact> phones = ContactData.getData("phone");
         List<Contact> emails = ContactData.getData("email");
 
-//        NavigableSet<Contact> sorted = new TreeSet<>(phones);
+//       If your element don't implement Comparable you must pass a comparator to the constructor.
         Comparator<Contact> mySort = Comparator.comparing(Contact::getName);
         NavigableSet<Contact> sorted = new TreeSet<>(mySort);
         sorted.addAll(phones);
@@ -20,16 +20,17 @@ public class TreeSetMain {
         phones.forEach(c -> justNames.add(c.getName()));
         System.out.println(justNames);
 
-        System.out.println(divider);
+        System.out.println(divider); // divider
 
         NavigableSet<Contact> fullSet = new TreeSet<>(sorted);
         fullSet.addAll(emails);
         fullSet.forEach(System.out::println);
 
-        System.out.println(divider);
+        System.out.println(divider); // divider
 
-        List<Contact> fullList = new ArrayList<>(phones);
-        fullList.addAll(emails);
+        // duplicates will be there
+        List<Contact> fullList = new ArrayList<>(phones); // get phones
+        fullList.addAll(emails); // get emails
         fullList.sort(sorted.comparator());
         fullList.forEach(System.out::println);
 
@@ -39,30 +40,78 @@ public class TreeSetMain {
         Contact first = fullSet.first();
         Contact last = fullSet.last();
 
-        System.out.println(divider);
+        System.out.println(divider); // divider
+
         System.out.printf("min = %s, first=%s %n", min.getName(), first.getName());
         System.out.printf("max = %s, last=%s %n", max.getName(), last.getName());
 
-        System.out.println(divider);
+        System.out.println(divider); // divider
 
         NavigableSet<Contact> copiedSet = new TreeSet<>(fullSet);
         System.out.println("First element = " + copiedSet.pollFirst()); // does the same but removes the element from the set also
         System.out.println("Last element = " + copiedSet.pollLast());
         copiedSet.forEach(System.out::println);
 
-        System.out.println(divider);
+        System.out.println(divider); // divider
 
         Contact daffy = new Contact("Daffy Duck");
         Contact daisy = new Contact("Daisy Duck");
         Contact snoopy = new Contact("Snoopy");
         Contact archie = new Contact("Archie");
 
-        for (Contact c : List.of(daffy, daisy, snoopy, archie)) {
+        // Contact first = fullSet.first(); --> first/last
+        for (Contact c : List.of(daffy, daisy, last, snoopy)) {
+            // ceiling --> greater than or equal to the element passed
             System.out.printf("ceiling(%s)=%s%n", c.getName(), fullSet.ceiling(c));
+            // higher --> always returns next greater value
             System.out.printf("higher(%s)=%s%n", c.getName(), fullSet.higher(c));
         }
 
-        System.out.println(divider);
+        System.out.println(divider); // divider
+
+        for (Contact c : List.of(daffy, daisy, first, archie)) {
+            // ceiling --> greater than or equal to the element passed
+            System.out.printf("floor(%s)=%s%n", c.getName(), fullSet.floor(c));
+            // higher --> always returns next greater value
+            System.out.printf("lower(%s)=%s%n", c.getName(), fullSet.lower(c));
+        }
+
+        System.out.println(divider); // divider
+
+        // descending sorted order
+        NavigableSet<Contact> descendingSet = fullSet.descendingSet();
+        descendingSet.forEach(System.out::println);
+
+        System.out.println(divider); // divider
+
+        Contact lastContact = descendingSet.pollLast(); // gets and remove the element
+        System.out.println("Removed " + lastContact);
+        descendingSet.forEach(System.out::println);
+        System.out.println(divider); // divider
+        fullSet.forEach(System.out::println);
+
+        System.out.println(divider); // divider
+
+        Contact marion = new Contact("Maid Marion");
+        // headSet = items before (like head → top part → earlier)
+        var headSet = fullSet.headSet(marion, true);
+        headSet.forEach(System.out::println);
+
+        System.out.println(divider); // divider
+
+        // tailSet = items after (like tail → back part → later)
+        var tailSet = fullSet.tailSet(marion, false);
+        tailSet.forEach(System.out::println);
+
+        System.out.println(divider); // divider
+
 
     }
 }
+
+/*
+Elements which implement Comparable (said to have a natural order sort,
+like strings and numbers) can be element of a TreeSet.
+
+If your element don't implement Comparable you must pass a comparator to the con0structor.
+ */
