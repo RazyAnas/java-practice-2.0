38af445ba7bd12cfb5bf89a10655e1715e74ac70
RazyAnas <razy7292@gmail.com>
2025-08-24 09:16:53 +0530

    Done

---
+++ src/LeetCodeSolutions/RemoveDuplicates.java
@@ -0,0 +1,24 @@
+package LeetCodeSolutions;
+
+import java.util.List;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+    }
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+    }
+
+}
