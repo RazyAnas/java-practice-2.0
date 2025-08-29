ffff8401f629ab6cb762213afd7a8857da95be3b
RazyAnas <razy7292@gmail.com>
2025-08-29 19:09:00 +0530

    Done

--- src/LeetCodeSolutions/RemoveDuplicatesFromSortedArray.java
+++ src/LeetCodeSolutions/RemoveDuplicatesFromSortedArray.java
@@ -9,21 +9,18 @@ public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
 
     }
-
     public int removeDuplicates(int[] arr) {
 
-        int left = 0;               // first pointer at start
-        int right = arr.length - 1; // second pointer at end
-        ArrayList list = new ArrayList<>(List.of(arr));
-        while (left < right) {
-            if (arr[left] == arr[right]) {
-                Arrays.asList(arr.clone()).remove(arr[right]);
+        if ((arr.length) == 0) return 0;
+        int size = arr.length;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] != arr[j] && j != i+1) {
+                    arr[j-1] = arr[j];
+                    size--;
+                }
             }
-            // move pointers inward
-            left++;
-            right--;
         }
-
-        return arr.length;
+        return size;
     }
 }
