d6a16fd44ae1037d84aaf16099b239bcd7d2e0d7
RazyAnas <razy7292@gmail.com>
2025-09-12 19:50:18 +0530

    Done

--- src/LeetCodeSolutions/GetFloorAndCeil.java
+++ src/LeetCodeSolutions/GetFloorAndCeil.java
@@ -19,21 +19,49 @@ public class GetFloorAndCeil {
         int[] result = new int[2];
         int ceil = Integer.MAX_VALUE;
         int floor = Integer.MIN_VALUE;
-        for (int num : nums) {
-            if (num >= x) {
-                // ceil
-                ceil = Math.min(ceil, num);
-                result[1] = ceil;
-            }
+        int low = 0;
+        int high = nums.length - 1;
 
-            if (num <= x) {
-                // floor
-                floor = Math.max(floor, num);
-                result[0] = floor;
+        // binary search for O(log n) time complexity
+        while (high >= low) {
+            int mid = (high + low)/2;
+            if (nums[mid] >= x) {
+                // ceil
+                ceil = Math.min(ceil, nums[mid]);
+                result[1] = ceil;
+                high = mid - 1;
             }
-            if (floor == Integer.MIN_VALUE) floor = -1; // no floor exists
-            if (ceil == Integer.MAX_VALUE) ceil = -1;   // no ceil exists
+            if (nums[mid] <= x) {
+                // floor
+                floor = Math.max(floor, nums[mid]);
+                result[0] = floor;
+                low = mid + 1;
+            }
         }
+
+
+
+
+
+
+        // Brute force
+//        for (int num : nums) {
+//            if (num >= x) {
+//                // ceil
+//                ceil = Math.min(ceil, num);
+//                result[1] = ceil;
+//            }
+//
+//            if (num <= x) {
+//                // floor
+//                floor = Math.max(floor, num);
+//                result[0] = floor;
+//            }
+//            if (floor == Integer.MIN_VALUE) floor = -1; // no floor exists
+//            if (ceil == Integer.MAX_VALUE) ceil = -1;   // no ceil exists
+//        }
+
+
         return result;
     }
 }
