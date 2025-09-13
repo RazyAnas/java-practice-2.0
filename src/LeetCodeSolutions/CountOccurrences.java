701c2f2951d197526a0e7a31d852f0e33dfd9b3d
RazyAnas <razy7292@gmail.com>
2025-09-13 16:20:40 +0530

    Done

---
+++ src/LeetCodeSolutions/CountOccurrences.java
@@ -0,0 +1,17 @@
+package LeetCodeSolutions;
+
+import java.util.Arrays;
+
+public class CountOccurrences {
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,1,1,1};
+        int target = 1;
+        System.out.println(countOccurrences(arr, target));
+
+    }
+    public static int countOccurrences(int[] arr, int target) {
+        // Your code goes here
+        // count = lastIndex - firstIndex + 1 --> formula
+        return 0;
+    }
+}
