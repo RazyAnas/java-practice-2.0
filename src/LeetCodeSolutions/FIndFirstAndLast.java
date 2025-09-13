c6e6667b49d5c00239fe3fe5ac6d96369892f64f
RazyAnas <razy7292@gmail.com>
2025-09-13 16:06:02 +0530

    Done

---
+++ src/LeetCodeSolutions/FIndFirstAndLast.java
@@ -0,0 +1,50 @@
+package LeetCodeSolutions;
+
+public class FIndFirstAndLast {
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findFirst(nums, target);
+        result[1] = findLast(nums, target);
+        return result;
+    }
+    public static int findFirst(int[] nums, int target) {
+        int ans = -1;
+        int low = 0;
+        int high = nums.length - 1;
+        while (high >= low) {
+            int mid = (high + low) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else if (nums[mid] > target) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static int findLast(int[] nums, int target) {
+        int ans = -1;
+        int low = 0;
+        int high = nums.length - 1;
+        while (high >= low) {
+            int mid = (high + low) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
